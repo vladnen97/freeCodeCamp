73dec60b11bb483bd65c56183eff51083dc771bd
vladnen97 <darkdescent1991@gmail.com>
2022-11-25 17:19:38 +0300

    Exercise num 22-25

---
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/14.js
@@ -0,0 +1,41 @@
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+/*Compound Assignment With Augmented Subtraction*/
+
+let i = 11;
+let j = 9;
+let k = 3;
+
+// Only change code below this line
+i -= 6;
+j -= 15;
+k -= -1;
+
+/*Compound Assignment With Augmented Multiplication*/
+
+let aM = 5;
+let bM = 12;
+let cM = 4.6;
+
+// Only change code below this line
+aM *= 5;
+bM *= 3;
+cM *= 10;
+
+/*Compound Assignment With Augmented Division*/
+
+let aD = 48;
+let bD= 108;
+let cD = 33;
+
+// Only change code below this line
+aD /=12;
+bD /=4;
+cD /=11;
