325ebdc3525d7c7ab7ea16af90791e42add2d66f
vladnen97 <darkdescent1991@gmail.com>
2022-11-25 18:12:43 +0300

    Exercise num 42-44

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/19.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/19.js
@@ -47,6 +47,25 @@ myArrayD[0] = 45;
 
 /*Access Multi-Dimensional Arrays With Indexes*/
 
+const myArrayE = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14],
+];
+
+const myDataE = myArrayE[2][1];
+
+/*Manipulate Arrays With push()*/
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3]);
+
+/*Manipulate Arrays With pop()*/
+
 
 
 
