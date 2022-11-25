9ea04623d7e5d785df2104185d61e960799b3a4f
vladnen97 <darkdescent1991@gmail.com>
2022-11-25 19:09:25 +0300

    Exercise num 49-57

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/20.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/20.js
@@ -26,6 +26,116 @@ timesFive(2);
 timesFive(0);
 
 /*Global Scope and Functions*/
+let myGlobal = 10;
+
+function fun1() {
+    oopsGlobal = 5; // Assign 5 to oopsGlobal Here
+
+}
+
+function fun2() {
+    let output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+
+fun2();
+
+/*Local Scope and Functions*/
+
+function myLocalScope() {
+    let myVar="test";
+    console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// console.log('outside myLocalScope', myVar);
+
+/*Global vs. Local Scope in Functions*/
+
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+    const outerWear = "sweater"
+    return outerWear;
+}
+myOutfit();
+
+/*Understanding Undefined Value returned from a Function*/
+//sum must be 8
+let sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+function addFive() {
+    sum+=5;
+}
+addThree();
+addFive();
+
+/*Assignment with a Returned Value*/
+let processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+processed = processArg(7);
+
+/*Stand in Line*/
+
+function nextInLine(arr, item) {
+    arr.push(item);
+    item = arr.shift();
+    return item;
+}
+let testArr = [1, 2, 3, 4, 5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+/*Understanding Boolean Values*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
