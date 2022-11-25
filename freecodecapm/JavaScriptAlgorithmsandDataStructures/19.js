7f0f0b249e4990875e88bce791c84aefb1329b6c
vladnen97 <darkdescent1991@gmail.com>
2022-11-25 18:07:54 +0300

    Exercise num 36-42

---
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/19.js
@@ -0,0 +1,67 @@
+/*Use Bracket Notation to Find the Last Character in a String*/
+// Setup
+const lastNameA = "Lovelace";
+
+// Only change code below this line
+const lastLetterOflastNameA = lastNameA[lastNameA.length-1]; // Change this line
+
+/*Use Bracket Notation to Find the Nth-to-Last Character in a String*/
+
+const lastNameB = "Lovelace";
+
+// Only change code below this line
+const lastLetterOflastNameB = lastNameB[lastNameB.length-4]; // Change this line
+
+/*Word Blanks*/
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myNoun +" test, " + myAdjective +  " . " + myVerb +" soo " + myAdverb; // Change this line
+
+/*Store Multiple Values in one Variable using JavaScript Arrays*/
+
+// Only change code below this line
+const myArrayA = ["string", 14];
+
+/*Nest one Array within Another Array*/
+
+// Only change code below this line
+const myArrayB = [["another array ", 14], ["another one "]];
+
+/*Access Array Data with Indexes*/
+
+const myArrayC = [50, 60, 70];
+let myDataC = myArrayC[0];
+
+/*Modify Array Data With Indexes*/
+
+// Setup
+const myArrayD = [18, 64, 99];
+
+// Only change code below this line
+myArrayD[0] = 45;
+
+/*Access Multi-Dimensional Arrays With Indexes*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
