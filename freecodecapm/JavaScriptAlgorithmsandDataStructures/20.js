5ff30fd9362752e9472735a856a42a5eca7252d3
vladnen97 <darkdescent1991@gmail.com>
2022-11-25 18:38:10 +0300

    Exercise num 49-51

---
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/20.js
@@ -0,0 +1,52 @@
+/*Write Reusable JavaScript with Functions*/
+
+function reusableFunction () {
+    console.log("Hi World");
+}
+
+reusableFunction();
+
+/*Passing Values to Functions with Arguments*/
+
+function functionWithArgs(sum1, sum2) {
+    console.log(sum1 + sum2);
+}
+
+functionWithArgs(1, 2);
+functionWithArgs(7, 9);
+
+/*Return a Value from a Function with Return*/
+
+function  timesFive(someValue) {
+    return someValue*5;
+}
+
+timesFive(5);
+timesFive(2);
+timesFive(0);
+
+/*Global Scope and Functions*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
