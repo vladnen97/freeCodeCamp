6e39adab3bdd07fead0a6c7067f24944da8dfb8d
vladnen97 <darkdescent1991@gmail.com>
2022-11-25 17:42:37 +0300

    Exercise num 32, 33, 34

---
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/18.js
@@ -0,0 +1,29 @@
+                                            /*Find the Length of a String*/
+
+// Setup
+let lastNameLength = 0;
+const lastNameA = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastNameA.length;
+
+                                                /*Bracket Notation*/
+
+// Setup
+let firstLetterOfLastName = "";
+const lastNameB = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastNameB[0]; // Change this line
+
+                                                /*String Immutability*/
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+
+
