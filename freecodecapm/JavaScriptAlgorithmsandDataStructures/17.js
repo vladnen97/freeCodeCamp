5f03397ac4309685fc9f64810bd63cf423a58ac5
vladnen97 <darkdescent1991@gmail.com>
2022-11-25 17:32:25 +0300

    Exercise num 30,31

---
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/17.js
@@ -0,0 +1,13 @@
+/*Constructing Strings with Variables*/
+
+const myName = "freeCodeCamp,";
+const myStr = "Hello, our name is " + myName + " how are you?";
+
+/*Appending Variables to Strings*/
+
+// Change code below this line
+const someAdjective = "hard";
+let myStrA = "Learning to code is ";
+
+myStrA +=someAdjective;
+
