cab6a53f279bc5e214b9300d0b1df050c5637e95
vladnen97 <darkdescent1991@gmail.com>
2022-11-25 17:27:03 +0300

    Exercise num 29

---
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/15.js
@@ -0,0 +1,8 @@
+/*Escaping Literal Quotes in Strings*/
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+const myStrWithASingleQuotes = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+const myStrA = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
---
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/16.js
@@ -0,0 +1,10 @@
+
+/*Concatenating Strings with Plus Operator*/
+
+const myStr = "This is" + " the start. This is the end."; // Change this line
+console.log(myStr);
+
+/*Concatenating Strings with the Plus Equals Operator*/
+
+let myStrPlusEquals = "This is the first sentence.";
+myStrPlusEquals += " This is the second sentence.";
