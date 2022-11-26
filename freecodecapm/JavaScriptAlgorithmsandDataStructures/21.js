a71ac86d728dcb6eef0bd6d647c486fef3e63d99
vladnen97 <darkdescent1991@gmail.com>
2022-11-26 13:38:36 +0300

    profile lookup

---
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/21.js
@@ -0,0 +1,47 @@
+const contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
+    for (let x = 0; x < contacts.length; x++) {
+        if (contacts[x].firstName === name) {
+            if (contacts[x].hasOwnProperty(prop)) {
+                return contacts[x][prop];
+            } else {
+                return "No such property";
+            }
+        }
+    }
+    return "No such contact";
+}
+
+console.log(lookUpProfile("Akira", "likes"));
+console.log(lookUpProfile("Akira", "address"));
+console.log(lookUpProfile("Bob", "potato"));
+console.log(lookUpProfile("Bob", "number"));
+console.log(lookUpProfile("Harry", "likes"));
+console.log(lookUpProfile("Sherlock", "likes"));
+console.log(lookUpProfile("Kristian", "lastName"));
