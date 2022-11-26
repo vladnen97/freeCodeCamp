ba83f41bef389644b1b65467f24afd41d57e117d
vladnen97 <darkdescent1991@gmail.com>
2022-11-26 12:25:47 +0300

    Exercise num 49-97

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/20.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/20.js
@@ -177,6 +177,67 @@ const myPlants = [
 ];
 const secondTree = myPlants[1].list[1];
 
+/*Albums*/
+
+const recordCollection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+        artist: 'Robert Palmer',
+        tracks: []
+    },
+    5439: {
+        albumTitle: 'ABBA Gold'
+    }
+};
+
+function updateRecords(records, id, prop, value) {
+    if (prop !== "tracks" &&  value !== "") {
+        records[id][prop] = value;
+    }
+    else if (prop === "tracks" && !records[id].hasOwnProperty(prop)) {
+        records[id][prop] = [value];
+    }
+    else if (prop === "tracks" && value !=="") {
+        records[id][prop].push(value);
+    }
+    else if (value === "") {
+        delete records[id][prop];
+    }
+    return records;
+}
+updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me")
+
+/*Iterate with JavaScript While Loops*/
+
+const myArrayI = [];
+let i = 5;
+while (i >= 0) {
+    myArrayI.push(i);
+    i--;
+}
+
+/*Iterate with JavaScript For Loops*/
+
+// Setup
+const myArrayJ = [];
+
+// Only change code below this line
+
+for (let i = 1; i < 6; i++) {
+    myArrayJ.push(i);
+}
+
+
+
 
 
 
