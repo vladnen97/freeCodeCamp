8430785f743f24d5c34963ba649eb3a49eb83d77
vladnen97 <darkdescent1991@gmail.com>
2022-11-27 18:26:15 +0300

    add exercises 1-9

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/RegExp/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/RegExp/01.js
@@ -16,5 +16,41 @@ let petString = "James has a pet cat.";
 let petRegex = /cat| dog| bird| fish/; // Change this line
 let resultC = petRegex.test(petString);
 
-/**/
+/*Ignore Case While Matching*/
+
+let myStringB = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let resultD = fccRegex.test(myStringB);
+
+/*EXtract matched string */
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let resultD = extractStr.match(codingRegex); // Change this line
+
+/*Find More Than the First Match*/
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; // Change this line
+let resultE = twinkleStar.match(starRegex); // Change this line
+
+/*Match Anything with Wildcard Period*/
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let resultE = unRegex.test(exampleStr);
+
+/*Match Single Character with Multiple Possibilities*/
+let quoteSampleA = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let resultF = quoteSampleA.match(vowelRegex); // Change this line
+
+/*Match Letters of the Alphabet*/
+
+let quoteSampleB = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let resultG = quoteSampleB.match(alphabetRegex); // Change this line
+
+/*Match Numbers and Letters of the Alphabet*/
+
+
 
