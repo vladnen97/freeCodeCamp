457683ef4721e1fddeb70d9bb535752e76a55516
vladnen97 <darkdescent1991@gmail.com>
2022-11-27 18:01:01 +0300

    add exercises 1-3

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/RegExp/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/RegExp/01.js
@@ -1,2 +1,20 @@
 /*Using the Test Method*/
 
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let resultA = myRegex.test(myString); // Change this line
+
+/* Match Literal Strings*/
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let resultB = waldoRegex.test(waldoIsHiding);
+
+/*Match a Literal String with Different Possibilities*/
+
+let petString = "James has a pet cat.";
+let petRegex = /cat| dog| bird| fish/; // Change this line
+let resultC = petRegex.test(petString);
+
+/**/
+
