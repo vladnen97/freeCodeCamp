d3da9e4abf71c939300cd90279a965668316d8b8
vladnen97 <darkdescent1991@gmail.com>
2022-11-27 17:44:54 +0300

    exercises 15 - 29

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/ES6/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/ES6/01.js
@@ -120,15 +120,12 @@ temp = thermos.temperature; // 26 in Celsius
 </html>
 
 /*Use export to Share a Code Block*/
-
 const uppercaseString = (string) => {
     return string.toUpperCase();
 }
-
 const lowercaseString = (string) => {
     return string.toLowerCase()
 }
-
 export {uppercaseString, lowercaseString };
 
 /*Reuse JavaScript Code Using import*/
@@ -140,6 +137,86 @@ uppercaseString("hello");
 lowercaseString("WORLD!");
 
 /*Use * to Import Everything from a File*/
+
+import * as stringFunctions from './string_functions.js';
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+/*Create an Export Fallback with export default*/
+export default function subtract(x, y) {
+    return x - y;
+}
+
+/*Import a Default Export*/
+
+import subtract from './math_functions.js';
+// Only change code above this line
+
+subtract(7,4);
+
+/*Create a JavaScript Promise*/
+
+const makeServerRequestA = new Promise((resolve, reject) => {
+
+});
+
+/*Complete a Promise with resolve and reject*/
+
+const makeServerRequestB = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+
+    if(responseFromServer) {
+        resolve("We got the data"); // Change this line
+    } else {
+        reject("Data not received"); // Change this line
+    }
+});
+
+/*Handle a Fulfilled Promise with "then"*/
+
+const makeServerRequestC = new Promise((resolve, reject) => {
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
+
+    if(responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+makeServerRequestC.then(result => console.log(result));
+
+/*Handle a Rejected Promise with catch*/
+
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to false to represent an unsuccessful response from a server
+    let responseFromServer = false;
+
+    if(responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+
+makeServerRequest.then(result => {
+    console.log(result);
+});
+
+makeServerRequest.catch(error => console.log(error));
+
+
+
+
+
+
+
+
+
+
 
 
 
