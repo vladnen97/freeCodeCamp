a52ec761044848f471ff9fb8d5aa66101dfeabc1
vladnen97 <darkdescent1991@gmail.com>
2022-11-28 21:48:58 +0300

    Use Caution When Reinitializing Variables Inside a Loop

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Debugging/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Debugging/01.js
@@ -13,12 +13,36 @@ let output = "Get this to show once in the freeCodeCamp console and not at all i
 console.log(output);
 console.clear();
 
-/*Use typeof to Check the Type of a Variable*/
+/*Use typeof to Check the Typeof a Variable*/
 
 let seven = 7;
 let three = "3";
 console.log(seven + three);
-// Only change code below this line
 console.log(typeof seven);
 console.log(typeof three);
+
+/*Use Caution When Reinitializing Variables Inside a Loop*/
+
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    let row = [];
+    for (let i = 0; i < m; i++) {
+        // Adds the m-th row into newArray
+
+        for (let j = 0; j < n; j++) {
+            // Pushes n zeroes into the current row to create the columns
+            row.push(0);
+        }
+        // Pushes the current row, which now has n zeroes in it, to the array
+        newArray.push(row);
+        row=[];
+    }
+    return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
+
 
