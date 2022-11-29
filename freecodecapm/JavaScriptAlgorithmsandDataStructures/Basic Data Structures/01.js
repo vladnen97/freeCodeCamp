e3318863ede7067e9e30357cebd6f2aa7a094263
vladnen97 <darkdescent1991@gmail.com>
2022-11-29 17:43:08 +0300

    Copy an Array with the Spread Operator

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Basic Data Structures/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Basic Data Structures/01.js
@@ -51,6 +51,17 @@ function forecast(arr) {
 }
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
 
+/*Copy an Array with the Spread Operator*/
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        newArr.push([...arr]);
+        num--;
+    }
+    return newArr;
+}
+console.log(copyMachine([true, false, true], 2));
+
 
 
 
