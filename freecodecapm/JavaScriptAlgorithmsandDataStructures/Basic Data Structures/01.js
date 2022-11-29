05cd6a86de6f80f13eff7f8c39943a6b142e86fe
vladnen97 <darkdescent1991@gmail.com>
2022-11-29 17:14:48 +0300

    Access an Array's Contents Using Bracket Notation

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Basic Data Structures/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Basic Data Structures/01.js
@@ -1,2 +1,11 @@
 /*Use an Array to Store a Collection of Data*/
 let yourArray = ["string", 5, true, {a: "darova", b:4}, null]; // Change this line
+
+/*Access an Array's Contents Using Bracket Notation*/
+
+let myArray = ["a", "e", "c", "d"];
+// Only change code below this line
+
+// Only change code above this line
+console.log(myArray[1]);
+
