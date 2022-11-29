cb146b42604a3497049e1231d0b988792a018b0d
vladnen97 <darkdescent1991@gmail.com>
2022-11-29 17:56:55 +0300

    Check For The Presence of an Element With indexOf()

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Basic Data Structures/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Basic Data Structures/01.js
@@ -70,6 +70,13 @@ function spreadOut() {
 }
 console.log(spreadOut());
 
+/*Check For The Presence of an Element With indexOf()*/
+
+function quickCheck(arr, elem) {
+    return arr.indexOf(elem) !== -1;
+}
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
 
 
 
