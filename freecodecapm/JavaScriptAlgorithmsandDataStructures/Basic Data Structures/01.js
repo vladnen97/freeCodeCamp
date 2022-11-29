d6d5ef0dafbfdc5237d8dd724f527a7e1ab1d5f9
vladnen97 <darkdescent1991@gmail.com>
2022-11-29 17:18:19 +0300

    Add Items to an Array with push() and unshift()

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Basic Data Structures/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Basic Data Structures/01.js
@@ -4,8 +4,16 @@ let yourArray = ["string", 5, true, {a: "darova", b:4}, null]; // Change this li
 /*Access an Array's Contents Using Bracket Notation*/
 
 let myArray = ["a", "e", "c", "d"];
-// Only change code below this line
-
-// Only change code above this line
 console.log(myArray[1]);
+
+/*Add Items to an Array with push() and unshift()*/
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift('I', 2, 'three');
+    arr.push(7, 'VIII', 9);
+    // Only change code above this line
+    return arr;
+}
+console.log(mixedNumbers(['IV', 5, 'six']));
+
 
