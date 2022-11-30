3838516032f294fc972b9dfb8fc293df6bf77386
vladnen97 <darkdescent1991@gmail.com>
2022-11-30 15:53:54 +0300

    Return Largest Numbers in Arrays

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Basic Algorithm Scripting/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Basic Algorithm Scripting/01.js
@@ -27,4 +27,20 @@ factorialize(5);
 let str = "The quick brown fox jumped over the lazy dog";
 str = str.split(" ");
 let longestWord = str.sort((a,b) => b.length - a.length);
-console.log(longestWord[0].length);
+
+/*Return Largest Numbers in Arrays*/
+
+function largestOfFour(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].sort((a, b) => b -a);
+        newArr.push(arr[i][0]);
+    }
+    return newArr;
+}
+
+console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+ console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
+ console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
+
+
