f17143e8a01a9fb73bdb16deba9973db09587803
vladnen97 <darkdescent1991@gmail.com>
2022-12-02 18:40:36 +0300

    Understand Own Properties

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Object Oriented Programming/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Object Oriented Programming/01.js
@@ -60,6 +60,21 @@ function House(numBedrooms) {
 let myHouse = new House(3);
 myHouse instanceof House;
 
+/*Understand Own Properties*/
+
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+let canary = new Bird("Tweety");
+let ownProps = [];
+
+for (let prop in canary) {
+    if(canary.hasOwnProperty(prop)) {
+        ownProps.push(prop);
+    }
+}
+
 
 
 
