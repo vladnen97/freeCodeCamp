e1b2bb57ba32394cb492566633c9ed0242ff092c
vladnen97 <darkdescent1991@gmail.com>
2022-12-05 22:54:14 +0300

    Implement the filter Method on a Prototype

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Functional Programming/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Functional Programming/01.js
@@ -1,12 +1,25 @@
 /*Implement map on a Prototype*/
 
 Array.prototype.myMap = function(callback) {
-    const newArray = [];
+    const newArrayA = [];
     for (let i=0; i < this.length; i++) {
-        newArray.push(callback(this[i], i, this));
+        newArrayA.push(callback(this[i], i, this));
     }
-    console.log(newArray);
+    // console.log(newArray);
 };
 
 [23, 65, 98, 5, 13].myMap(item => item * 2);
 [1, 1, 2, 5, 2].myMap((element, index, array) => array[index + 1] || array[0])
+
+
+/*Implement the filter Method on a Prototype*/
+
+Array.prototype.myFilter = function(callback) {
+    const newArray = [];
+    for (let i=0; i < this.length; i++) {
+        if (callback(this[i], i, this))  newArray.push(this[i]);
+    }
+    // console.log(newArray);
+};
+[23, 65, 98, 5, 13].myFilter(item => item % 2);
+["naomi", "quincy", "camperbot"].myFilter(element => element === "naomi");
