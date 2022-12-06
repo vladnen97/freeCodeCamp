91ac4c2552939a50c2854a3427218c7744c55187
vladnen97 <darkdescent1991@gmail.com>
2022-12-06 23:55:56 +0300

    Diff Two Arrays

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Intermediate Algorithm Scripting/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Intermediate Algorithm Scripting/01.js
@@ -3,7 +3,7 @@
 function sumAll(arr) {
     let sum =0;
     arr =arr.sort((a, b) => a-b);
-    for (let i = arr[0]; i < arr[1] + 1; i++) {
+    for (let i = arr[0]; i <= arr[1]; i++) {
         sum+= i;
     }
     return sum;
@@ -11,3 +11,27 @@ function sumAll(arr) {
 
 // console.log(sumAll([1, 4]));
 // console.log(sumAll([10, 5]));
+
+/*Diff Two Arrays*/
+
+function diffArray(arr1, arr2) {
+    const newArr = [];
+
+    for (let i=0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) === -1) {
+            newArr.push(arr2[i]);
+        }
+    }
+    for (let i=0; i < arr1.length; i++) {
+        if (arr2.indexOf(arr1[i]) === -1) {
+            newArr.push(arr1[i]);
+        }
+    }
+    console.log(newArr);
+}
+
+diffArray([7, 2, 3, 5], [1, 2, 3, 4, 5]);
+diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
+
+
+
