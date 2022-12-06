b93c889301713789968d5c2d26697ce8a26a8bc1
vladnen97 <darkdescent1991@gmail.com>
2022-12-06 05:49:21 +0300

    Combine an Array into a String Using the join Method

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Functional Programming/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Functional Programming/01.js
@@ -69,3 +69,10 @@ function splitify(str) {
     return str.split(/\W/gi);
 }
 splitify("Hello World,I-am code");
+
+/*Combine an Array into a String Using the join Method*/
+
+function sentensify(str) {
+    return str.split(/\W/gi).join(" ");
+}
+sentensify("May-the-force-be-with-you");
