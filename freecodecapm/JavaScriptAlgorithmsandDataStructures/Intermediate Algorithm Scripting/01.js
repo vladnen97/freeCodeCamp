54dbc6e037362cd42e6a6d4af8ffdf6fabc0c579
vladnen97 <darkdescent1991@gmail.com>
2022-12-08 02:07:43 +0300

    Seek and Destroy

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Intermediate Algorithm Scripting/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Intermediate Algorithm Scripting/01.js
@@ -2,8 +2,9 @@
 
 function sumAll(arr) {
     let sum =0;
-    arr =arr.sort((a, b) => a-b);
-    for (let i = arr[0]; i <= arr[1]; i++) {
+    let minA = arr[0];
+    let maxA = arr[1];
+    for (let i = minA; i <= maxA; i++) {
         sum+= i;
     }
     return sum;
@@ -27,11 +28,23 @@ function diffArray(arr1, arr2) {
             newArr.push(arr1[i]);
         }
     }
-    console.log(newArr);
+    return newArr;
 }
 
 diffArray([7, 2, 3, 5], [1, 2, 3, 4, 5]);
 diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]);
 
+/*Seek and Destroy*/
 
+function destroyer(arr, ...args) {
+    let test = [...args];
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!test.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
 
