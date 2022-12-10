df19ea9c47e16f097fdc3014d0cfd449718d9249
vladnen97 <darkdescent1991@gmail.com>
2022-12-10 05:58:43 +0300

    Missing letters

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Intermediate Algorithm Scripting/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Intermediate Algorithm Scripting/01.js
@@ -120,3 +120,19 @@ function pairElement(str) {
 
 pairElement("ATCGA");
 pairElement("TTGAG");
+
+/*Missing letters*/
+
+function fearNotLetter(str) {
+    for (let i = 0; i < str.length; i++) {
+
+        const charCode = str.charCodeAt(i);
+
+        if (charCode !== str.charCodeAt(0) + i) {
+            return String.fromCharCode(charCode - 1);
+        }
+    }
+    return undefined;
+}
+
+console.log(fearNotLetter("abce"));
