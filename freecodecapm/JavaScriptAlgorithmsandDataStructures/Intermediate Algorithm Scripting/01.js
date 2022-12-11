4d8c194414ebfa9c164929312186e56da1baccf7
vladnen97 <darkdescent1991@gmail.com>
2022-12-11 06:19:10 +0300

    Binary Agents

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Intermediate Algorithm Scripting/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Intermediate Algorithm Scripting/01.js
@@ -220,4 +220,21 @@ function steamrollArray(arr) {
     }, []);
 }
 
-console.log(steamrollArray([1, [2], [3, [[4]]]]));
+// console.log(steamrollArray([1, [2], [3, [[4]]]]));
+
+/*Binary Agents*/
+
+function binaryAgent(str) {
+    let charStr = "", arr =str.split(" ");
+    for (let i =0; i < arr.length; i++) {
+        charStr += String.fromCharCode(parseInt(arr[i], 2));
+    }
+    return charStr;
+}
+
+// console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000" +
+//                             " 01100010 01101111 01101110 01100110 01101001 01110010 01100101" +
+//                             " 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+
+
+
