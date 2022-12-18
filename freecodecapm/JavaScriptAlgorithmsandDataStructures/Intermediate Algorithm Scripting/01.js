75a225312b1d1bb0975270a34506476ebcb4ad21
vladnen97 <darkdescent1991@gmail.com>
2022-12-18 06:37:44 +0300

    Map the Debris

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/Intermediate Algorithm Scripting/01.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/Intermediate Algorithm Scripting/01.js
@@ -332,4 +332,13 @@ function sumPrimes(num) {
 
 // console.log(sumPrimes(10));
 
+/*Map the Debris*/
+
+function orbitalPeriod(arr) {
+    const GM = 398600.4418;
+    const earthRadius = 6367.4447;
+    return arr.map(({name, avgAlt}) => ({name, orbitalPeriod: Math.round(2*Math.PI* Math.sqrt(Math.pow(earthRadius+avgAlt, 3)/GM))}));
+}
+
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
 
