a37543e64001469d620ab4d88d34a8073d3d0472
vladnen97 <darkdescent1991@gmail.com>
2022-12-19 10:18:47 +0300

    Telephone Number Validator

--- freecodecapm/JavaScriptAlgorithmsandDataStructures/JavaScriptAlgorithmsAndDataStructuresProjects/TelephoneNumberValidator.js
+++ freecodecapm/JavaScriptAlgorithmsandDataStructures/JavaScriptAlgorithmsAndDataStructuresProjects/TelephoneNumberValidator.js
@@ -1,7 +1,7 @@
 /*Telephone Number Validator*/
 
 function telephoneCheck(str) {
-    return "";
+    return /^1?\s?((\(\d{3})\)|([ -]?\d{3})){2}((\(\d{4}\))|([ -]?\d{4}))$/g.test(str);
 }
 
 console.log(telephoneCheck("555-555-5555"));
